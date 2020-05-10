41b41c2bb09552eb7cb2330be32e6003a958357d
Alicia Carlson <ajcarlson@student.fullsail.edu>
2020-05-10 19:22:35 -0400

    Deleted and restarted project from Windows to be able to use File IO to pull IP address from dedicated file

--- Carlson_Alicia_DatabaseReview/Carlson_Alicia_DatabaseReview/Program.cs
+++ Carlson_Alicia_DatabaseReview/Carlson_Alicia_DatabaseReview/Program.cs
@@ -3,20 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
 namespace Carlson_Alicia_DatabaseReview
 {
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
-            
-
-
         }
     }
 }
