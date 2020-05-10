d94803080acd816585157d66c5a7a30cdec1cc73
Alicia Carlson <ajcarlson@student.fullsail.edu>
2020-05-10 19:15:29 -0400

    Created Visual Studio project for database review assignment

---
+++ Carlson_Alicia_DatabaseReview/Carlson_Alicia_DatabaseReview/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Carlson_Alicia_DatabaseReview
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
