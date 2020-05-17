2794a1d4edf75858a73d2fdc9036150e4569ac2e
Alicia Carlson <ajcarlson@student.fullsail.edu>
2020-05-17 00:57:18 -0400

    Refactored code. Fixed the math issue so total is displayed correctly. Added validation class and validated all user input through said class. Cleaned up output formatting.

--- Carlson_Alicia_jsPractice/C# Projects/Project3_Arrays_BookBuyer_C#/Velasco_Alicia_Arrays/Program.cs
+++ Carlson_Alicia_jsPractice/C# Projects/Project3_Arrays_BookBuyer_C#/Velasco_Alicia_Arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Velasco_Alicia_Arrays
 {
@@ -10,81 +11,43 @@ namespace Velasco_Alicia_Arrays
              * SDI 1910 Section 01
              * Assignment : Arrays
              * 10 / 13 / 19
+             * Refactored 05 / 16 / 2020
              */
 
             //Problem #1 - Book Buyer
 
             //Setup variables
-            String booksToBuyString;
             int booksToBuy;
-            String bookPriceString;
             decimal bookPrices;
+            decimal total = 0;
 
 
             //Tell the user what we're doing and prompt for number of books the user wants to buy
             Console.WriteLine("Let's calculate the price of the books you'd like to buy.\r\n" +
                 "Start by entering in the number of books to buy.");
 
-            //Catch user response
-            booksToBuyString = Console.ReadLine();
-
-            //validation
-            while (!(int.TryParse(booksToBuyString, out booksToBuy)) && booksToBuy > 0)
-            {
-                //Tell the user the problem
-                Console.WriteLine("Please only enter a whole number in number form.");
-
-                //Restate the question
-                Console.WriteLine("How many books are you buying?");
-
-                //Recatch the user's response
-                booksToBuyString = Console.ReadLine();
-            }
+            // User's response with validation
+            booksToBuy = Validation.GetInt(Console.ReadLine());
 
             //Declare the array
             decimal[] booksWereBuying = new decimal[booksToBuy];
-
 
             //for loop to catch user input - book prices
             for (int i = 0; i < booksWereBuying.Length; i++)
             {
                 //Asking the user for the book prices
-                Console.WriteLine("Please enter the price of the book you'd like to buy.");
-                bookPriceString = Console.ReadLine();
+                Console.WriteLine("Please enter the price of book {0}.", i + 1);
+                // User's response with validation
+                bookPrices = Validation.GetDecimal(Console.ReadLine());
 
-
-                //Convert and validate book prices
-                while (!decimal.TryParse(bookPriceString, out bookPrices))
-                {
-                    
-                    //Tell the user the problem
-                    Console.WriteLine("Please enter only numbers");
-
-                    //Restate the question
-                    Console.WriteLine("How much is the price of the book?");
-
-                    //Recatch the user's response
-                    bookPriceString = Console.ReadLine();
-
-                }
-
+                // Assign values to array elements
                 booksWereBuying[i] = bookPrices;
 
-                //if loop to add the array
-                if (i < booksWereBuying.Length)
-                {
-                    decimal totalPrice = booksWereBuying[i]++;
-                    
+                // Add current iteration of book price to total
+                total += booksWereBuying[i];
 
-
-
-                    //output results to user
-                    Console.WriteLine("The total for {0} books is ${1}", i + 1, totalPrice);
-
-                    //entering blank line
-                    Console.WriteLine("");
-
-                }
+                // Output number of books and total price to the user
+                Console.WriteLine("The total for {0} books is ${1}\n", i + 1, total);
             }
 
 
@@ -136,75 +99,75 @@ namespace Velasco_Alicia_Arrays
 
 
 
-            //Problem #2 -
+            ////Problem #2 -
 
-            //Declare and define arrays
+            ////Declare and define arrays
 
-            string[] randomThings = new string[5];
+            //string[] randomThings = new string[5];
 
-            randomThings[0] = "grapes";
-            randomThings[1] = "apples";
-            randomThings[2] = "limes";
-            randomThings[3] = "lemons";
-            randomThings[4] = null;
+            //randomThings[0] = "grapes";
+            //randomThings[1] = "apples";
+            //randomThings[2] = "limes";
+            //randomThings[3] = "lemons";
+            //randomThings[4] = null;
 
-            string[] colors = new string[5];
-            colors[0] = "purple";
-            colors[1] = "red";
-            colors[2] = "green";
-            colors[3] = "yellow";
-            colors[4] = null;
-
-
-            //Cycle through the arrays outputting the results
-            for (int i = 0; i < randomThings.Length; i++)
-            {
-                Console.WriteLine("\r\nThe main color of {0} is {1}.", randomThings[i], colors[i]);
-
-            }
+            //string[] colors = new string[5];
+            //colors[0] = "purple";
+            //colors[1] = "red";
+            //colors[2] = "green";
+            //colors[3] = "yellow";
+            //colors[4] = null;
 
 
-            //update array with new values
-            randomThings[0] = "ball";
-            randomThings[1] = "carrot";
-            randomThings[2] = "towel";
-            randomThings[3] = "laptop";
-            randomThings[4] = "stove";
+            ////Cycle through the arrays outputting the results
+            //for (int i = 0; i < randomThings.Length; i++)
+            //{
+            //    Console.WriteLine("\r\nThe main color of {0} is {1}.", randomThings[i], colors[i]);
 
-            colors[0] = "red";
-            colors[1] = "orange";
-            colors[2] = "white";
-            colors[3] = "silver";
-            colors[4] = "black";
+            //}
 
 
-            //cycle through the arrays outputting the results
-            for (int i = 0; i < colors.Length; i++)
-            {
-                Console.WriteLine("The main color of {0} is {1}.", randomThings[i], colors[i]);
+            ////update array with new values
+            //randomThings[0] = "ball";
+            //randomThings[1] = "carrot";
+            //randomThings[2] = "towel";
+            //randomThings[3] = "laptop";
+            //randomThings[4] = "stove";
 
-            }
+            //colors[0] = "red";
+            //colors[1] = "orange";
+            //colors[2] = "white";
+            //colors[3] = "silver";
+            //colors[4] = "black";
 
-            /*Problem #2 - Coloring Outside the Lines Test
-             *
-             * Test 1:
-             * The main color of grapes is purple.
 
-                The main color of apples is red.
+            ////cycle through the arrays outputting the results
+            //for (int i = 0; i < colors.Length; i++)
+            //{
+            //    Console.WriteLine("The main color of {0} is {1}.", randomThings[i], colors[i]);
 
-                The main color of limes is green.
+            //}
 
-                The main color of lemons is yellow.
-             *
-             *
-             * Test 2:
-             * The main color of  is .
-                The main color of ball is red.
-                The main color of carrot is orange.
-                The main color of towel is white.
-                The main color of laptop is silver.
-                The main color of stove is black.
-             */
+            ///*Problem #2 - Coloring Outside the Lines Test
+            // *
+            // * Test 1:
+            // * The main color of grapes is purple.
+
+            //    The main color of apples is red.
+
+            //    The main color of limes is green.
+
+            //    The main color of lemons is yellow.
+            // *
+            // *
+            // * Test 2:
+            // * The main color of  is .
+            //    The main color of ball is red.
+            //    The main color of carrot is orange.
+            //    The main color of towel is white.
+            //    The main color of laptop is silver.
+            //    The main color of stove is black.
+            // */
 
 
 
---
+++ Carlson_Alicia_jsPractice/C# Projects/Project3_Arrays_BookBuyer_C#/Velasco_Alicia_Arrays/Validation.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Velasco_Alicia_Arrays
+{
+    public class Validation
+    {
+        public static float GetFloat(string input, string message = "Please enter a vaild number: ")
+        {
+            float validatedFloat;
+
+
+            while (!float.TryParse(input, out validatedFloat))
+            {
+                Console.Write("Please enter a vaild number: ");
+                input = Console.ReadLine();
+
+            }
+
+            return validatedFloat;
+
+        }
+
+
+        public static int GetInt(string input)
+        {
+            int validatedInt;
+
+
+            while (!int.TryParse(input, out validatedInt))
+            {
+                Console.Write("Please enter a vaild number: ");
+                input = Console.ReadLine();
+
+            }
+
+            return validatedInt;
+
+        }
+
+        public static int GetInt(int min, int max, string input)
+        {
+            int validatedInt;
+
+
+            while (!int.TryParse(input, out validatedInt) || (validatedInt < min || validatedInt > max))
+            {
+                Console.Write("Please enter a vaild response: ");
+                input = Console.ReadLine();
+
+            }
+
+            return validatedInt;
+
+        }
+        public static double GetDouble(string input)
+        {
+            double validatedDouble;
+
+
+            while (!double.TryParse(input, out validatedDouble))
+            {
+                Console.Write("Please enter a vaild response: ");
+                input = Console.ReadLine();
+
+            }
+
+            return validatedDouble;
+
+        }
+
+        public static decimal GetDecimal(string input)
+        {
+            decimal validatedDecimal;
+
+
+            while (!decimal.TryParse(input, out validatedDecimal))
+            {
+                Console.Write("Please enter a vaild number: ");
+                input = Console.ReadLine();
+
+            }
+
+            return validatedDecimal;
+        }
+
+
+            public static string NotNullOrBlank(string input, string message)
+        {
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Please do not leave blank.");
+                Console.Write(message);
+                input = Console.ReadLine();
+
+            }
+
+            return input;
+
+        }
+
+        public static string NotNullOrBlankYesOrNo(string input, string message)
+        {
+            while (string.IsNullOrWhiteSpace(input) && (input.ToLower() != "y" && input.ToLower() != "n"))
+            {
+                Console.WriteLine("Please do not leave blank and only enter y / n");
+                Console.Write(message);
+                input = Console.ReadLine();
+
+            }
+
+            return input;
+
+        }
+
+        public static string NotNullOrBlank3Vars(string input, string first, string second, string third, string message)
+        {
+            while (string.IsNullOrWhiteSpace(input) && (input != first && input != second && input != third))
+            {
+                Console.WriteLine($"Please do not leave blank and only enter {first} / {second} / {third}: ");
+                Console.Write(message);
+                input = Console.ReadLine();
+
+            }
+
+            return input;
+        }
+
+        public static void PauseBeforeContinuing(string message = "\n\nPress any key to return to Main Menu")
+        {
+            Console.Write($"\n\n{message}");
+            Console.ReadKey();
+        }
+
+
+    }
+}
