a2c3dc4ca4d4b8b2c6b4d4927391e72a68ce4edf
Alicia Carlson <ajcarlson@student.fullsail.edu>
2020-05-20 15:20:47 -0400

    Started final project in C#. Created OpeningDisplay method.

---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpeningDisplay();
+        }
+
+        public static void OpeningDisplay()
+        {
+            using (StreamReader inStream = new StreamReader(@"/Users/aliciajo/Desktop/Carlson_Alicia_Portfolio/FinalProject/FinalProject/openingDisplay.txt"))
+            {
+                while (inStream.Peek() > -1)
+                {
+                    string line = inStream.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+
+            Validation.PauseBeforeContinuing("Press any key to begin...");
+        }
+    }
+}
